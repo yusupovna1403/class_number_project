5026e927d5efad4f68972023ed3bc559044aabb3
djumanov <diyorbekjumanov1120@gmail.com>
2022-12-09 23:17:04 +0500

    done

---
+++ README.md
@@ -0,0 +1,26 @@
+# Assignment for Object Oriented Programming
+
+## Create a class "Number"
+
+Attributes:
+
+* `number` (integer)
+
+Methods:
+
+* `__init__` (constructor)
+* `get_number` (returns the number)
+* `is_even` (returns True if the number is even, False otherwise)
+* `is_odd` (returns True if the number is odd, False otherwise)
+* `is_prime` (returns True if the number is prime, False otherwise)
+* `get_divisors` (returns a list of all divisors of the number)
+* `get_length` (returns the number of digits of the number)
+* `get_sum` (returns the sum of all digits of the number)
+* `get_reverse` (returns the number in reverse order)
+* `get_digits` (returns a list of all digits of the number)
+* `get_max` (returns the largest digit of the number)
+* `get_min` (returns the smallest digit of the number)
+* `get_average` (returns the average of all digits of the number)
+* `get_median` (returns the median of all digits of the number)
+* `get_range` (returns the range of all digits of the number)
+* `get_frequency` (returns a dictionary with the frequency of each digit)
---
+++ number.py
@@ -0,0 +1,137 @@
+class Number:
+    def __init__(self, value):
+        self.value = value
+
+
+    def get_number(self):
+        """
+        Returns the number.
+
+        returns: int
+        """
+        pass
+
+    def is_odd(self):
+        """
+        Returns True if the number is odd, otherwise False.
+
+        returns: bool
+
+        """
+        pass
+
+    def is_even(self):
+        """
+        Returns True if the number is even, otherwise False. 
+
+        returns: bool
+        """
+        pass
+
+    def is_prime(self):
+        """
+        Returns True if the number is prime, otherwise False.
+
+        returns: bool
+        """
+        pass
+
+    def get_divisors(self):
+        """
+        Returns a list of all the divisors of the number.
+
+        returns: list
+        """
+        pass
+
+    def get_length(self):
+        """
+        Returns the number of digits in the number.
+
+        returns: int
+        """
+        pass
+
+    def get_sum(self):
+        """
+        Returns the sum of all the digits in the number.
+
+        returns: int
+        """
+        pass
+
+    def get_reverse(self):
+        """
+        Returns the number in reverse.
+
+        returns: int
+        """
+        pass
+
+    def is_palindrome(self):
+        """
+        Returns True if the number is a palindrome, otherwise False.
+
+        returns: bool
+        """
+        pass
+
+    def get_digits(self):
+        """
+        Returns a list of all the digits in the number.
+
+        returns: list
+        """
+        pass
+
+    def get_max(self):
+        """
+        Returns the largest digit in the number.
+
+        returns: int
+        """
+        pass
+
+    def get_min(self):
+        """
+        Returns the smallest digit in the number.
+
+        returns: int
+        """
+        pass
+
+    def get_average(self):
+        """
+        Returns the average of all the digits in the number.
+
+        returns: float
+        """
+        pass
+
+    def get_median(self):
+        """
+        Returns the median of all the digits in the number.
+
+        returns: float
+        """
+        pass
+
+    def get_range(self):
+        """
+        Returns the range of all the digits in the number.
+
+        returns: list
+        """
+        pass
+
+    def get_frequency(self):
+        """
+        Returns a dictionary of the frequency of each digit in the number.
+
+        returns: dict
+        """
+        pass
+    
+
+# Create a new instance of Number
+number = Number(3)
